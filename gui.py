96a857184d0a2c6f988a232c5e0f2dfb380879fb
mulwasteveh <stephenmulwa344@gmail.com>
2021-01-18 07:42:05 +0300

    tkinter window

--- gui.py
+++ gui.py
@@ -1,6 +1,25 @@
 from tkinter import *
 
-steve = Tk()
-steve.title("gui")
-steve.geometry("300x300")
-steve.mainloop()
+gui = Tk()
+gui.title('LOGIN PAGE')
+gui.geometry("500x400")
+gui.configure(bg='blue')
+
+
+l1= Label(gui, text='ENTER YOUR FIRST NAME')
+l1.grid(column= 0, row = 0, padx=20)
+
+l2 = Label(gui, text='ENTER YOUR SECOND NAME')
+l2.grid(column=0, row=1)
+
+l3 = Label(gui, text='ENTER YOUR SURNAME')
+l3.grid(column=0, row=2)
+
+
+gui.mainloop()
+
+
+
+
+
+
